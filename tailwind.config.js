085ae33a499731f4704d1c9e237fbbc42d3904a8
Matheus <matheusdecarvalho100@gmail.com>
2024-10-14 20:39:53 -0300

    Dia_14_10_2024

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,7 @@ module.exports = {
     fontFamily:{
       'sans': ['Roboto', 'sans']
     },
-    extend: {
-      backgroundImage: {
-        "home": "url('/assets/bg.png')"
-      },
+    
       keyframes: {
         fundomechendo: {
           '0%': { backgroundPosition: '0% 0%' },
@@ -18,8 +15,8 @@ module.exports = {
       },
       animation: {
         fundomechendo: 'fundomechendo 100s infinite ease-in-out'
-      }
-    },
+      },
+    
   },
   plugins: [],
 }
