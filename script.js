0fef3fe3ae7f3f481d0a19e51c6ed92f8adccc61
Matheus De Carvalho <matheusdecarvalho100@gmail.com>
2024-10-15 10:59:22 -0300

    Update script.js

--- script.js
+++ script.js
@@ -198,7 +198,7 @@ checkoutBtn.addEventListener("click", function () {
 function checkRestaurantOpen() {
     const data = new Date()
     const hora = data.getHours()
-    return hora >= 18 && hora < 22; //true = restaurante esta aberto
+    return hora >= 7 && hora < 22; //true = restaurante esta aberto
 
 }
 
